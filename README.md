a29805faadcbf28d16e058fa80c3c94fbb62b453
Josselin Bodan <jbodan@orange.fr>
2019-05-08 22:18:56 +0200

    :sparkles: app

---
+++ README.md
@@ -0,0 +1,5 @@
+# Nexmo SMS Receiver
+Client pour recevoir des mails depuis
+
+- [NODEJS](https://nodejs.org/en/)
+- [GMAIL](https://mail.google.com/mail/u/0/)
--- index.js
+++ index.js
@@ -1,10 +1,57 @@
 const express = require('express')
 const app = express()
 
+const dotenv = require('dotenv')
+dotenv.config()
+
+const nodemailer = require('nodemailer')
+const transporter = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  secure: false,
+  requireTLS: true,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASSWORD
+  }
+})
+
 app.get('/', function (req, res){
   res.send('Hello World!')
 })
 
+app.get('/webhooks/inbound-sms', (req, res) => {
+
+  const from = req.query.msisdn
+  const to = req.query.to
+  const text = req.query.text
+
+  const nexmo_res = {
+    msisdn: from,
+    to: to,
+    text: text
+  }
+
+  console.log(process.env.MAIL_USER + process.env.MAIL_PASSWORD)
+
+  const mailOptions = {
+    from: process.env.MAIL_USER + '<'+process.env.MAIL_PASSWORD+'>',
+    to: process.env.MAIL_TO,
+    subject: 'NEXMO || INCOME SMS',
+    text: text
+  }
+
+  transporter.sendMail(mailOptions, function(err, info){
+    if(err)
+      console.log(err)
+  })
+
+  console.log(nexmo_res)
+
+  res.sendStatus(204)
+
+})
+
 app.listen(3000, function(){
-  console.log('runing on port 3000')
+  console.log('runing on port ' + process.env.PORT)
 })
