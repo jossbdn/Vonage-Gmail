44b7d0ec95cb254e2db39ce58d325bfb563f4282
Josselin Bodan <jbodan@orange.fr>
2019-05-08 14:42:55 +0200

    :bug:

--- index.php
+++ index.php
@@ -4,15 +4,17 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require 'vendor/autoload.php';
 
-
 $app = new \Slim\App;
 $handler = function (Request $request, Response $response) {
-    $client = new Nexmo\Client(new Nexmo\Client\Credentials\Basic('8dc33401', 'Dmo3cqYq52BRhhy1'));
-    $message = $client->message()->send([
-        'to' => '33670181541',
-        'from' => '33670181541',
-        'text' => $request->getParsedBody()
-    ]);
+    $params = $request->getParsedBody();
+
+    // Fall back to query parameters if needed
+    if (!count($params)){
+        $params = $request->getQueryParams();
+    }
+
+    error_log(print_r($params, true));
+    return $response->withStatus(204);
 };
 
 $app->get('/webhooks/delivery-receipt', $handler);
